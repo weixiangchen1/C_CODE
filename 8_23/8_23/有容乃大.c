619ea2220250707072f9f5caf9fffe16cf84676c
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2021-08-23 20:07:43 +0800

    Add files via upload

---
+++ 8_23/8_23/有容乃大.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+    int val = printf("Hello World!");
+    printf("\n");
+    printf("%d\n", val);
+
+    return 0;
+}
