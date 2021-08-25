2475ca28fdc8c9e88799b902e6138adcb96c6f5f
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2021-08-25 23:45:55 +0800

    Add files via upload

---
+++ 8_25/8_25/数据的存储.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	//char a = 128;
+//	//00000000000000000000000010000000
+//	//10000000
+//	int i = -10;
+//	unsigned int j = 10;
+//	printf("%u\n", i);
+//	
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	*pFloat = 9.0;
+//	printf("num的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//unsigned char i = 0;
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+	char a[1000];
+	int i;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	printf("%d", strlen(a));
+	return 0;
+}
