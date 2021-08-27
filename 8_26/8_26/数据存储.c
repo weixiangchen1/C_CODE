2860ba6348782fc40abbb3efcfa6de69eaa85eda
weixiangchen1 <86154924+weixiangchen1@users.noreply.github.com>
2021-08-27 22:03:04 +0800

    Add files via upload

---
+++ 8_26/8_26/数据存储.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = -10;
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int check_sys()
+//{
+//	int i = 1;
+//	return (*(char*)&i);
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char a = 3;
+//	char b = 127;
+//	char c = a + b;
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;
+//	printf("a=%d,b=%d,c=%d", a, b, c);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	return 0;
+}
